ffd7b77520833dfa6c0bf9e89e5cc3fd958c197d
Hafid Ali Mustaqim <hafidd456@gmail.com>
2021-07-01 08:00:55 +0700

    day2 : operator

---
+++ day2/day2.kt
@@ -0,0 +1,45 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function accepts following parameters:
+ *  1. DOUBLE meal_cost
+ *  2. INTEGER tip_percent
+ *  3. INTEGER tax_percent
+ */
+
+fun solve(meal_cost: Double, tip_percent: Int, tax_percent: Int): Unit {
+    // Write your code here
+    val tip = (meal_cost * tip_percent) / 100
+    val tax = (meal_cost * tax_percent) / 100
+    val total = meal_cost + tip + tax
+    println(Math.round(total))
+}
+
+fun main(args: Array<String>) {
+    val meal_cost = readLine()!!.trim().toDouble()
+
+    val tip_percent = readLine()!!.trim().toInt()
+
+    val tax_percent = readLine()!!.trim().toInt()
+
+    solve(meal_cost, tip_percent, tax_percent)
+}
